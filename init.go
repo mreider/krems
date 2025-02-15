9a2c34861ea19838b8d0f1279c4349af42e4fb60
Matthew Reider <mreider@gmail.com>
2025-02-15 19:02:56 +0100

    mollusk init works

--- init.go
+++ init.go
@@ -21,6 +21,7 @@ func handleInit() {
 	// Create basic structure
 	dirs := []string{
 		"markdown",
+		"markdown/topics",
 		"markdown/css",
 		"markdown/js",
 		"markdown/images",
@@ -89,9 +90,57 @@ func handleInit() {
 		os.Exit(1)
 	}
 
+	indexMD := `---
+title: "All About Mollusks"
+image: "images/mollusk.png"
+---
+
+Welcome to our sample site about mollusks! Just a short piece of text here
+about snails and slugs and other interesting creatures. 
+`
+    err := os.WriteFile("markdown/index.md", []byte(indexMD), 0644)
+    if err != nil {
+        fmt.Printf("Error writing markdown/index.md: %v\n", err)
+        os.Exit(1)
+    }
+    fmt.Println("Created: markdown/index.md")
+
+    // Subdirectory page: topics/index.md
+    topicsMD := `---
+title: "Mollusk Topics"
+image: "images/mollusk2.png"
+---
+
+Here we discuss more detailed topics related to mollusks in a subdirectory.
+Short text again for demonstration.
+`
+    err = os.WriteFile("markdown/topics/index.md", []byte(topicsMD), 0644)
+    if err != nil {
+        fmt.Printf("Error writing markdown/topics/index.md: %v\n", err)
+        os.Exit(1)
+    }
+    fmt.Println("Created: markdown/topics/index.md")
+
+
+	configYAML := `# Example config for local test site
+website:
+  url: "http://localhost:8080"
+  name: "Local Test Site"
+
+menu:
+  - title: "Home"
+    path: "index.md"
+  - title: "Topics"
+    path: "topics/index.md"
+`
+    if err := os.WriteFile("config.yaml", []byte(configYAML), 0644); err != nil {
+        fmt.Printf("Error writing config.yaml: %v\n", err)
+        os.Exit(1)
+    }
+    fmt.Println("Created: config.yaml")
 	fmt.Println("\nYour sample site structure has been created!")
 	fmt.Println("Next steps:")
 	fmt.Println("  1) Modify the markdown content in ./markdown")
-	fmt.Println("  2) Edit config.yaml as needed (website name, url, menu, etc.)")
+	fmt.Println("  2) Edit config.yaml as needed")
 	fmt.Println("  3) Run 'krems --build' to generate your static site!")
 }
